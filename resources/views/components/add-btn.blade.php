fdfdcd7c8f785f0ae199571d9c25c99ed47f422f
shaileshhq <shaileshhq@gmail.com>
2026-02-28 11:41:35 +0530

    feat: add new Blade component for a reusable add button.

--- resources/views/components/add-btn.blade.php
+++ resources/views/components/add-btn.blade.php
@@ -1,1 +1,3 @@
-<a class="btn btn-primary btn-xs btn-icon-text float-end" href="{{$route}}" title="{{$text}}" wire:navigate><i class="btn-icon-prepend bi bi-plus-square"></i>{{$text}}</a>
+<a class="btn btn-primary btn-xs btn-icon-text float-end" href="{{$route}}" title="{{$text}}" wire:navigate>
+    <i class="btn-icon-prepend bi bi-plus-square"></i>{{$text}}
+</a>
